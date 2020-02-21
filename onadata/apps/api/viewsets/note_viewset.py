a822623c938552457dcd080a27db811fc8b3623a
Davis Raymond Muro <davisraymondmuro@outlook.com>
2020-02-21 16:39:37 +0300

    Install and utilize djangorestframework-guardian
    Install djangorestframework-guardian to provide guardian support
    for DjangoRestFramework. Guardian support functions are no longer
    present within djangorestframework and have been moved into the
    djangorestframework-guardian package. More Info:
    https://github.com/encode/django-rest-framework/blob/4863a24451e2df3f2f6b5ba24b8a161bfa95390f/docs/community/3.9-announcement.md#djangoobjectpermissionsfilter-moved-to-third-party-package

--- onadata/apps/api/viewsets/note_viewset.py
+++ onadata/apps/api/viewsets/note_viewset.py
@@ -1,5 +1,5 @@
 from rest_framework import status
-from rest_framework.filters import DjangoObjectPermissionsFilter
+from rest_framework_guardian.filters import DjangoObjectPermissionsFilter
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
