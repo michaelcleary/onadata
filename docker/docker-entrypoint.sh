0bb658cef816120aecdece3c0c2b59f214119d02
kenhorn <ken.horn@gmail.com>
2020-03-09 10:57:28 +0000

    assume rds has setup onadata db, use default user/pass from terraform

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,8 +3,8 @@
 sleep 20
 
 #psql -h $FORMHUB_DB_SERVER -U postgres -c "CREATE ROLE onadata WITH SUPERUSER LOGIN PASSWORD '$PGPASSWORD';"
-psql -h $FORMHUB_DB_SERVER -U postgres -c "CREATE ROLE onadata LOGIN PASSWORD '$PGPASSWORD';"
-psql -h $FORMHUB_DB_SERVER -U postgres -c "CREATE DATABASE onadata OWNER onadata;"
+#psql -h $FORMHUB_DB_SERVER -U postgres -c "CREATE ROLE onadata LOGIN PASSWORD '$PGPASSWORD';"
+#psql -h $FORMHUB_DB_SERVER -U postgres -c "CREATE DATABASE onadata OWNER onadata;"
 psql -h $FORMHUB_DB_SERVER -U postgres onadata -c "CREATE EXTENSION postgis; CREATE EXTENSION postgis_topology;"
 
 virtualenv -p `which $SELECTED_PYTHON` /srv/onadata/.virtualenv/${SELECTED_PYTHON}
